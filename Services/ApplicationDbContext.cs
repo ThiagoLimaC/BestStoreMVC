5596bf94d4bb0b69c51dd35f3a1d505dc6883f4c
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-04-30 17:06:13 -0300

    Add Product model and initial migration for database
    Added a `Product` model with properties and validation annotations.
    Updated `ApplicationDbContext` to include a `DbSet<Product>` for
    database interaction. Created an initial migration to define the
    `Products` table schema, including constraints and metadata.
    Updated the model snapshot and added necessary `using` statements.

--- Services/ApplicationDbContext.cs
+++ Services/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BestStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BestStoreMVC.Data
 {
@@ -8,5 +9,7 @@ namespace BestStoreMVC.Data
         {
             
         }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
